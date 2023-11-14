e38cd3a06267694dbc680096764cff0b62353acd
Aaron Adetunmbi <adetunmbikenny@gmail.com>
2023-11-14 14:34:45 +0100

    started creating questions data

---
+++ src/utils/data.ts
@@ -0,0 +1,204 @@
+const questions = [
+  {
+    question: "You have a difficult mission ahead. How do you approach it?",
+    options: [
+      "Charge in headfirst",
+      "Strategize and plan",
+      "Seek guidance from others",
+    ],
+  },
+  {
+    question: "Your friend is in trouble. What's your immediate reaction?",
+    options: [
+      "Rush to help without a second thought",
+      "Assess the situation and form a plan",
+      "Consult others for advice",
+    ],
+  },
+  {
+    question: "How do you deal with setbacks or failures?",
+    options: [
+      "Keep pushing forward with determination",
+      "Reflect on what went wrong and adjust",
+      "Seek advice from friends or mentors",
+    ],
+  },
+  {
+    question: "In a team, what role do you naturally take on?",
+    options: [
+      "Frontline attacker",
+      "Strategist and planner",
+      "Support and mediator",
+    ],
+  },
+  {
+    question: "Your favorite activity during downtime is:",
+    options: [
+      "Training or physical activity",
+      "Reading or studying",
+      "Spending time with friends",
+    ],
+  },
+  {
+    question: "How do you express your feelings?",
+    options: [
+      "Openly and passionately",
+      "Thoughtfully and calmly",
+      "Reserved and subtly",
+    ],
+  },
+  {
+    question: "Your friend has a different opinion. How do you handle it?",
+    options: [
+      "Engage in a passionate debate",
+      "Listen and find a compromise",
+      "Avoid confrontation and maintain harmony",
+    ],
+  },
+  {
+    question: "During a mission, your team is in disagreement. What do you do?",
+    options: [
+      "Take charge and make decisions",
+      "Facilitate a discussion to find a consensus",
+      "Seek guidance from a higher authority",
+    ],
+  },
+  {
+    question: "Your ideal mission partner would be someone who:",
+    options: [
+      "Shares your enthusiasm and energy",
+      "Is a strategic thinker and planner",
+      "Is reliable and supportive",
+    ],
+  },
+  {
+    question: "How do you handle stress or pressure?",
+    options: [
+      "Confront it head-on with determination",
+      "Analyze the situation and find a solution",
+      "Seek support and advice from others",
+    ],
+  },
+  {
+    question: "In a team, what quality do you value the most?",
+    options: [
+      "Enthusiasm and energy",
+      "Intelligence and strategy",
+      "Cooperation and harmony",
+    ],
+  },
+  {
+    question: "Your approach to training is:",
+    options: [
+      "Intensive physical training",
+      "Focused on learning new techniques",
+      "Balancing physical and mental exercises",
+    ],
+  },
+  {
+    question: "How do you feel about leadership roles?",
+    options: [
+      "Embrace and enjoy leading others",
+      "Willing to lead when necessary",
+      "Prefer supporting roles",
+    ],
+  },
+  {
+    question: "Your preferred fighting style is:",
+    options: [
+      "Aggressive and dynamic",
+      "Tactical and strategic",
+      "Adaptive and supportive",
+    ],
+  },
+  {
+    question: "What motivates you the most?",
+    options: [
+      "Personal goals and ambitions",
+      "Intellectual pursuits and growth",
+      "Maintaining harmony and relationships",
+    ],
+  },
+  {
+    question: "When faced with a tough decision, you tend to:",
+    options: [
+      "Trust your instincts and gut feeling",
+      "Analyze the situation and weigh options",
+      "Seek advice from others for guidance",
+    ],
+  },
+  {
+    question: "Your response to someone challenging your beliefs is:",
+    options: [
+      "Defend your beliefs passionately",
+      "Engage in a calm and logical discussion",
+      "Avoid confrontation and maintain peace",
+    ],
+  },
+  {
+    question: "What do you value the most in a friendship?",
+    options: [
+      "Loyalty and shared experiences",
+      "Intellectual connection and understanding",
+      "Harmony and mutual support",
+    ],
+  },
+  {
+    question: "Your reaction to a surprise attack is:",
+    options: [
+      "React quickly with instinctive moves",
+      "Assess the situation and formulate a counter-strategy",
+      "Rely on teamwork and coordination",
+    ],
+  },
+  {
+    question: "Your long-term goal is to:",
+    options: [
+      "Become the best in your field",
+      "Acquire knowledge and wisdom",
+      "Foster strong bonds with others",
+    ],
+  },
+  {
+    question: "How do you deal with competition",
+    options: [
+      "Embrace it and strive to be the best",
+      "See it as an opportunity for growth",
+      "Minimize competition and focus on collaboration",
+    ],
+  },
+  {
+    question: "Your reaction to witnessing injustice is",
+    options: [
+      "Take immediate action to correct it",
+      "Analyze the situation and plan a strategic response",
+      "Seek guidance on the best course of action",
+    ],
+  },
+  {
+    question: "Your communication style is described as",
+    options: [
+      "Expressive and passionate",
+      "Thoughtful and articulate",
+      "Reserved and concise",
+    ],
+  },
+  {
+    question: "In a crisis, you prioritize",
+    options: [
+      "Taking charge and providing direction",
+      "Collaborating with others to find solutions",
+      "Maintaining emotional stability and supporting others",
+    ],
+  },
+  {
+    question: "Your preferred leisure activity is",
+    options: [
+      "Engaging in physical activities or sports",
+      "Reading, studying, or learning new skills",
+      "Spending quality time with friends and loved ones",
+    ],
+  },
+];
+
+console.log(questions.length);
